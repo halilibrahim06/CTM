fd7d9ced7850e6a9cfe213ff778405580bc7671c
halilibrahim06 <halilibrahimerdogan006@gmail.com>
2020-10-26 21:53:47 +0000

    pom change

--- src/test/java/com/ctmTest/runners/CukesRunner.java
+++ src/test/java/com/ctmTest/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ctmTest/step_definitions",
         dryRun = false,
-        tags = "@smoke"
+        tags = "@001"
 )
 public class CukesRunner {
 }
